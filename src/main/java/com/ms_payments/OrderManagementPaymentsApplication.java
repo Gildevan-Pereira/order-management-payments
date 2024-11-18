a5ee62998e3a8ff91bd0ca686e89b0ee741e628b
GildevanPereira <gildevanpereira.pro@gmail.com>
2024-11-17 21:06:29 -0300

    first-commit

---
+++ src/main/java/com/ms_payments/OrderManagementPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.ms_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms_payments/OrderManagementPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
