61146f499deef1f0491565ad583d448c6e760727
GildevanPereira <gildevanpereira.pro@gmail.com>
2024-12-15 16:33:57 -0300

    feature/ajustando o docker compose

--- src/main/java/com/ms_payments/repository/PaymentsHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ms_payments.repository;
-
-import com.ms_payments.model.document.PaymentsHistoryDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PaymentsHistoryRepository extends MongoRepository<PaymentsHistoryDocument, String> {
-}
