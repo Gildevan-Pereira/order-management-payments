77bf8723174aa221dd3e77222734b7c26ed7b344
GildevanPereira <gildevanpereira.pro@gmail.com>
2024-12-18 20:30:47 -0300

    feature/criando feature-flag com redis e ativacao e desativacao por endpoint

--- src/main/java/com/ms_payments/featureflag/FeatureFlag.java
+++
@@ -1,8 +0,0 @@
-package com.ms_payments.featureflag;
-
-public interface FeatureFlag {
-
-    boolean isFeatureEnabled(String featureName);
-
-    void updateFeatureFlag(String featureName, boolean enabled);
-}
